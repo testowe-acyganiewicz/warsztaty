1fa06ce873c68f09f0ce0d773644139f21b9bf34
Adrian Zieba <adrian.zieba@infakt.pl>
2015-10-13 14:10:03 +0200

    Moduły i wyjątki

---
+++ moduly_wyjatki/2_wyjatki.rb
@@ -0,0 +1,3 @@
+# obecnie obsługujemy problem dotyczący dzielenia przez 0 za pomocą if'a, zmieńmy tą implementację na obsługę za pomocą wyjątków
+# na początek sprawdźmy jaki wyjątek jest rzucany w momencie gdy dzielimy przez zero
+# nastepnie za pomocą bloku begin, rescue, end obsłużmy ten wyjątek
